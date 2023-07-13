7290c69752cbcc91c0e0be72a563175e225f6fd1
Henry <henry.guerrero0229@utc.edu.ec>
2023-07-12 20:36:01 -0500

    feat(UX/UI): mejorado el Navbar, botones de tipos de hoteles

--- app/components/navbar/Navbar.jsx
+++ app/components/navbar/Navbar.jsx
@@ -7,7 +7,7 @@ import BusquedaCentral from './busquedaCentral/BusquedaCentral'
 const Navbar = () => {
     return (
         <>
-            <div class="grid grid-cols-12 gap-4 py-8">
+            <div class="grid grid-cols-12 gap-4 py-6">
                 <div class="flex justify-center col-span-3">
                     <Link href='/' passHref>
                         <Image src={Logo} alt='StayFinder logo' priority className='w-72' />
@@ -16,9 +16,9 @@ const Navbar = () => {
                 <div class="flex justify-center col-span-6">
                     <BusquedaCentral />
                 </div>
-                <div class="flex justify-center col-span-3">
+                {/* <div class="flex justify-center col-span-3">
                     <Image src={Logo} alt='StayFinder logo' priority className='w-72' />
-                </div>
+                </div> */}
             </div>
             <hr className='border-[#ccc]' />
         </>
--- app/components/tipos-hoteles/item/Item.jsx
+++ app/components/tipos-hoteles/item/Item.jsx
@@ -14,14 +14,14 @@ const Item = ({ name, label }) => {
     const [hovered, setHovered] = useState(false);
 
     return (
-        <div className="cursor-pointer h-[80px] w-[90px] my-7 rounded-[10px] bg-[#fcfcfc] flex flex-col justify-center items-center hover:bg-[#5db299] transition-all ease-in-out" onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)}>
-            {name === 'economicos' && <IconoEconomicos hovered={hovered} />}
-            {name === 'huespedes' && <IconoHuespedes hovered={hovered} />}
-            {name === 'boutique' && <IconoBoutique hovered={hovered} />}
-            {name === 'BB' && <IconoBB hovered={hovered} />}
-            {name === 'lujo' && <IconoLujo hovered={hovered} />}
-            {name === 'resort' && <IconoResort hovered={hovered} />}
-            <small className='text-center block text-[#b9b9b9] text-xs'>{label}</small>
+        <div className={`cursor-pointer h-[80px] w-[90px] my-7 rounded-[10px] bg-[#fcfcfc] flex flex-col justify-center items-center hover:bg-[#5db299] transition-all ease-in-out ${hovered && 'shadow-xl hover:scale-105'}`} onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)}>
+            {name === 'economicos' && <IconoEconomicos hovered={hovered} className="transition-all ease-in-out" />}
+            {name === 'huespedes' && <IconoHuespedes hovered={hovered} className="transition-all ease-in-out" />}
+            {name === 'boutique' && <IconoBoutique hovered={hovered} className="transition-all ease-in-out" />}
+            {name === 'BB' && <IconoBB hovered={hovered} className="transition-all ease-in-out" />}
+            {name === 'lujo' && <IconoLujo hovered={hovered} className="transition-all ease-in-out" />}
+            {name === 'resort' && <IconoResort hovered={hovered} className="transition-all ease-in-out" />}
+            <small className={`text-center block text-[#b9b9b9] text-xs transition-all ease-in-out ${hovered && 'text-white'}`}>{label}</small>
         </div>
     )
 }
