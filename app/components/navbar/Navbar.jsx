49d12978350f4c57b87b0c00520cd7bc4559c429
Henry <henry.guerrero0229@utc.edu.ec>
2023-07-16 11:20:44 -0500

    fix(Navbar): reemplazando class por className

--- app/components/navbar/Navbar.jsx
+++ app/components/navbar/Navbar.jsx
@@ -7,16 +7,16 @@ import BusquedaCentral from './busquedaCentral/BusquedaCentral'
 const Navbar = () => {
     return (
         <>
-            <div class="grid grid-cols-12 gap-4 py-6">
-                <div class="flex justify-center col-span-3">
+            <div className="grid grid-cols-12 gap-4 py-6">
+                <div className="flex justify-center col-span-3">
                     <Link href='/' passHref>
                         <Image src={Logo} alt='StayFinder logo' priority className='w-72' />
                     </Link>
                 </div>
-                <div class="flex justify-center col-span-6">
+                <div className="flex justify-center col-span-6">
                     <BusquedaCentral />
                 </div>
-                {/* <div class="flex justify-center col-span-3">
+                {/* <div className="flex justify-center col-span-3">
                     <Image src={Logo} alt='StayFinder logo' priority className='w-72' />
                 </div> */}
             </div>
