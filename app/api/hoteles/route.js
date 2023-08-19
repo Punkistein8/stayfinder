a14c5edce71f488315c97455b04030d33a6ef02a
Henry <henry.guerrero0229@utc.edu.ec>
2023-08-19 14:37:50 -0500

    fix(deploy):fixing para deploy 6

--- app/api/hoteles/route.js
+++ app/api/hoteles/route.js
@@ -3,16 +3,11 @@ import { PrismaClient } from "@prisma/client";
 
 const stayfinder = new PrismaClient();
 
-export async function GET(request) {
+export async function GET() {
     try {
         const data = await stayfinder.hoteles.findMany()
         return NextResponse.json(data)
     } catch (error) {
         throw new Error(error);
     }
-}
-
-export async function POST(req, res) {
-    const body = await req.json()
-    return NextResponse.json({ text: `From Post, Body: ${body.text}` })
 }
